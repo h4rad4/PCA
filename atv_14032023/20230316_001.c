49a9056003f5387bd8bf5c68100c5728cc6c99d3
h4rad4 <williamharad@outlook.com>
2023-03-23 04:24:19 -0400

    .

--- atv_14032023/20230316_001.c
+++ atv_14032023/20230316_001.c
@@ -1,7 +1,7 @@
 /*
 ------------------------------------------------------------------------------------------------------------------------------------
-Crie uma função que aloca e lê um vetor de n inteiros. Crie outra função que recebe o vetor e retorna o maior e o menor valor. 
-O programa principal deve executar as duas funções, imprimir os valores retornados pela 2ª função e liberar a memória alocada pela 
+Crie uma função que aloca e lê um vetor de n inteiros. Crie outra função que recebe o vetor e retorna o maior e o menor valor.
+O programa principal deve executar as duas funções, imprimir os valores retornados pela 2ª função e liberar a memória alocada pela
 1ª função.
 ------------------------------------------------------------------------------------------------------------------------------------
 */
@@ -11,11 +11,50 @@ O programa principal deve executar as duas funções, imprimir os valores retorn
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int *v;
+    int qt;
+
+    printf("Insira a quantidade de elementos do vetor. ");
+    scanf("%i", &qt);
+
+    // Alocando e lendo o vetor
+    inputs(&v, qt);
+
+    // Liberando a memoria alocada
+    free(v);
+
     return 0;
 }
 
-int inputs()
+int inputs(int *v, int qt)
 {
-    ;
+    v = (int *)malloc(qt * sizeof(int));
+
+    for (int i = 0; i < qt; i++)
+    {
+        printf("Insira o elemento %i do vetor. ", i);
+        scanf("%d", &*(v + i));
+
+        // printf("%d ", *(v+i));
+    }
+
+    comparate(v, qt);
+}
+
+int comparate(int **v, int qt)
+{
+    int maior = *v;
+    int menor = *v;
+
+    for (int i = 0; i < qt; i++)
+    {
+        if (*(v + i) > maior)
+            maior = *(v + i);
+
+        if (*(v + i) < menor)
+            menor = *(v + i);
+    }
+
+    printf("Maior valor = %d\n", maior);
+    printf("Menor valor = %d", menor);
 }
